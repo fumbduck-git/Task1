2860532da4cdb5144db71f26ea04b5ae25047b3c
Harsha <h4rsh4op@gmail.com>
2024-07-22 14:20:22 +0530

    Update hardcodedvariables.py

--- Task-1-main/hardcodedvariables.py
+++ Task-1-main/hardcodedvariables.py
@@ -1,14 +1,18 @@
 import matplotlib.pyplot as plt
-a = 0.01
-b = -0.5
-c = 25
-temperature = 20.0
-pressure = 800.0
-humidity = 79.0
+
+a = 0.015
+b = -0.7 
+c = 20 
+
+temperature = 22.0  
+humidity = 75.0    
+
 precipitation = a * temperature**2 + b * humidity + c
 print(f"Precipitation: {precipitation}")
+
 temperature_range = range(0, 31)
 precipitation_values = [a * temp**2 + b * humidity + c for temp in temperature_range]
+
 plt.figure(figsize=(8, 6))
 plt.plot(temperature_range, precipitation_values, marker='o', linestyle='-')
 plt.xlabel('Temperature (°C)')
